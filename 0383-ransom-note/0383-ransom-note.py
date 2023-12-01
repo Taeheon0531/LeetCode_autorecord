bd59d478bbb9114f1c6d45f17625945916e075ed
Taheon Lee <92841001+Taeheon0531@users.noreply.github.com>
2023-12-01 15:19:14 +0900

    Time: 70 ms (20.44%), Space: 13.4 MB (52.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
+
+# counter란 dictionary 로 개수를 세어줌
+# from collections import counter
